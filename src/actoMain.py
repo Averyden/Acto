db76a93dd93166df99de94c5149be63812a2102a
Averyden <you@example.com>
2024-02-22 09:33:10 +0100

    Applied rewrite

--- src/actoMain.py
+++ src/actoMain.py
@@ -1,72 +1,162 @@
 import sys
-sys.dont_write_bytecode = True
+sys.dont_write_bytecode = True #* Don't write pycache.
 
 import tkinter as tk
 import tkinter.ttk as ttk
+from datetime import datetime
 from data.actoData import actoData
 
-data = actoData()
-
-data.createTables()
-#data.deleteAction(1)
 
 #? Maybe set window to a constant size?
 
 class TkinterApp(ttk.Frame):
-    def __init__(self,master=None):
-        ttk.Frame.__init__(self, master)
-        self.placeholder = None
-
+    def __init__(self,root):
+        #* For priority
         self.emptyStar = "☆"
         self.fullStar = "★"
         self.priorityRating = "0"
+
+       
+        self.root = root
+        self.root.title("Acto")
+        self.data = actoData()
         
-        #* Build the UI on load.
-        self.buildStartUI()
+        self.data.createTables() #* In case there are no tables.
+
+        self.notebook = ttk.Notebook(self.root)
+        self.notebook.pack(fill="both", expand=True)
+
+        self.uncompletedTabFrame = ttk.Frame(self.notebook)
+        self.completedTabFrame = ttk.Frame(self.notebook)
+        self.createActionTabFrame = ttk.Frame(self.notebook)
+
+
+        self.notebook.add(self.uncompletedTabFrame, text="Uncompleted Actions")
+        self.notebook.add(self.completedTabFrame, text="Completed Actions")
+        self.notebook.add(self.createActionTabFrame, text="Create Action")
+
+        self.buildCompletedUI()
+        self.buildUncompletedUI()
+        self.buildCreationUI()
+
+        self.updateLabels()
+
+      
+    def updateLabels(self): #* Automatically run every time code is started so that things are added to the ma bobs
+        l = self.data.getActionList()
+        self.lblActions.config(text = 'There are {} uncompleted Actions'.format(len(l)))
+        self.db_view.delete(*self.db_view.get_children())
+        for a in l:
+            self.db_view.insert("", tk.END, values=(a.actionID, a.content, "", self.getPriority(a.priority)))
+
+    def getPriority(self, number):
+        return "★" * number + "☆" * (5 - number)
 
     def buildUncompletedUI(self):
-        self.Ui = tk.Tk()
-        self.dataPanel = tk.Frame(self.Ui)
-        self.butPanel = tk.Frame(self.Ui)
-        self.settingPanel = tk.Frame(self.butPanel)
+        self.dataPanel = ttk.Frame(self.uncompletedTabFrame)
+        self.butPanel = ttk.Frame(self.uncompletedTabFrame)
+        self.settingPanel = ttk.Frame(self.butPanel)
+        self.lblActions = ttk.Label(self.butPanel, text = 'There are no uncompleted Actions')
+        self.lblActions.grid(row = 0, column = 0)
+        self.butUpdate = ttk.Button(self.butPanel, text = 'Edit Action', command=None)
+        self.butUpdate.grid(row = 4, column = 0)
+       
+        self.lblCurrentSelect = ttk.Label(self.butPanel, text="No currently selected Action.")
+        self.lblCurrentSelect.grid(row=0, column=4, columnspan=2, padx=25)
+        self.butOpen = ttk.Button(self.butPanel, text="Open Action", command=None)
+        self.butOpen.grid(row = 1, column = 2, columnspan=2, padx=25)
+        
 
-        self.lblSelectedAction = tk.Label(self.butPanel, text="No currently selected action to modify.")
-        self.lblSelectedAction.grid(row=1, column=2)
+        self.butNew = ttk.Button(self.butPanel, text = 'Create Action', command = self.buildCreationUI)
+        self.butNew.grid(row=4, column=2, columnspan = 2)
 
-        self.db_view = ttk.Treeview(self.data_panel, column=("column1", "column2", "column3", "column4", "column5"), show='headings')
-       # self.db_view.bind("<<TreeviewSelect>>", lambda event: self.onOrderSelect(self.db_view.item(self.db_view.selection())['values'][0]))
-        self.db_view.column("#1", width=70)
+        
+        self.butCompleteActions = ttk.Button(self.butPanel, text = "Mark as uncompleted", command=None)
+        self.butCompleteActions.grid(row=4, column=4,columnspan=4)
+         
+   
+
+        self.db_view = ttk.Treeview(self.dataPanel, column=("column1", "column2", "column3", "column4"), show='headings')
+        #self.db_view.bind("<ButtonRelease-1>", self.on_guitar_selected)
+        self.db_view.column("#1", width=30)
         self.db_view.heading("#1", text="ID")
         self.db_view.heading("#2", text="Action")
         self.db_view.heading("#3", text="Deadline")
         self.db_view.heading("#4", text="Priority")
-        self.db_view["displaycolumns"]=("column1", "column4", "column3", "column2")
-        ysb = ttk.Scrollbar(self.data_panel, command=self.db_view.yview, orient=tk.VERTICAL)
+        self.db_view["displaycolumns"]=("column1", "column2", "column3", "column4")
+        ysb = ttk.Scrollbar(self.dataPanel, command=self.db_view.yview, orient=tk.VERTICAL)
         self.db_view.configure(yscrollcommand=ysb.set)
         self.db_view.pack(side = tk.TOP)
 
-        self.settingPanel.grid(row=2, column=0)
-        
         self.dataPanel.pack(side = tk.TOP)
         self.butPanel.pack(side = tk.LEFT)
-        self.pack()
-        self.Ui.mainloop()
+        self.settingPanel.grid(row=2, column=2)
       
         
+    def buildCreationUI(self):
+        #* Variables for the action
+        self.actionContentEntry = ttk.Entry(self.createActionTabFrame)
+        self.actionContentEntry.grid(row=1, column=0, columnspan=2)
 
-    
+        # self.lblDeadlineHeader = ttk.Label(self.createActionTabFrame, text="Set deadline (yyyy-mm-dd)")
+        # self.lblDeadlineHeader.grid(row=2, column=0)
+
+        self.lblPriorityHeader = ttk.Label(self.createActionTabFrame, text="Set priority.")
+        self.lblPriorityHeader.grid(row=2, column=1, columnspan=3, padx=125)
+
+        # self.deadlineEntry = ttk.Entry(self.createActionTabFrame)
+        # self.deadlineEntry.grid(row=3, column=1)
+
+        # self.butSetDeadline = ttk.Button(self.createActionTabFrame, text="Set Deadline", command=self.setDeadline)
+        # self.butSetDeadline.grid(row=4, column=0)
+
+        self.slPriority = ttk.LabeledScale(self.createActionTabFrame, from_ = 0, to = 5)
+        #self.scPris.config(showvalue=1)
+        self.slPriority.grid(row = 3, column = 2)
+
+        # Button to create action
+        self.createActionButton = ttk.Button(self.createActionTabFrame, text="Create Action", command=self.createAction)
+        self.createActionButton.grid(row=5, column=0, columnspan=7)
+
+    # def setDeadline(self):
+    #     # Set self.deadline to the date entered in the entry
+    #     deadline_str = self.deadlineEntry.get()
+    #     try:
+    #         self.deadline = datetime.strptime(deadline_str, '%Y-%m-%d').date()
+    #     except ValueError:
+    #         print("Invalid date format. Please enter date in yyyy-mm-dd format.")
+
+    def createAction(self):
+        # Get content from entry
+        self.content = self.actionContentEntry.get()
+        self.priority = int(self.slPriority.scale.get())
+        # Call data's createAction method
+        self.data.createAction(content=self.content, priority=self.priority) #*Deadlines are put on hold for an indefinete time.
+        self.updateLabels()
+
+
     def buildCompletedUI(self):
-        self.Ui = tk.Tk()
-        self.dataPanel = tk.Frame(self.Ui)
-        self.butPanel = tk.Frame(self.Ui)
-        self.settingPanel = tk.Frame(self.butPanel)
 
-        self.lblSelectedAction = tk.Label(self.butPanel, text="No currently selected action to modify.")
-        self.lblSelectedAction.grid(row=1, column=2)
+        self.dataPanel = ttk.Frame(self.completedTabFrame)
+        self.butPanel = ttk.Frame(self.completedTabFrame)
+        self.settingPanel = ttk.Frame(self.butPanel)
+        self.lblActions = ttk.Label(self.butPanel, text = 'There are {} completed Actions'.format(None))
+        self.lblActions.grid(row = 0, column = 0)
 
-        self.db_view = ttk.Treeview(self.dataPanel, column=("column1", "column2", "column3", "column4", "column5"), show='headings')
-       # self.db_view.bind("<<TreeviewSelect>>", lambda event: self.onOrderSelect(self.db_view.item(self.db_view.selection())['values'][0]))
-        self.db_view.column("#1", width=70)
+        self.lblCurrentSelect = ttk.Label(self.butPanel, text="No currently selected Action.")
+        self.lblCurrentSelect.grid(row=0, column=2, columnspan=2, padx=25)
+        self.butOpen = ttk.Button(self.butPanel, text="Open Action", command=None)
+        self.butOpen.grid(row = 1, column = 2, columnspan=2)
+
+        
+        self.butUncompleteAction = ttk.Button(self.butPanel, text = "Mark as uncompleted", command=None)
+        self.butUncompleteAction.grid(row=4, column=4,columnspan=4, padx=25)
+         
+   
+
+        self.db_view = ttk.Treeview(self.dataPanel, column=("column1", "column2", "column3", "column4"), show='headings')
+        #self.db_view.bind("<ButtonRelease-1>", self.on_guitar_selected)
+        self.db_view.column("#1", width=30)
         self.db_view.heading("#1", text="ID")
         self.db_view.heading("#2", text="Action")
         self.db_view.heading("#3", text="Deadline")
@@ -76,43 +166,16 @@ class TkinterApp(ttk.Frame):
         self.db_view.configure(yscrollcommand=ysb.set)
         self.db_view.pack(side = tk.TOP)
 
-        self.settingPanel.grid(row=2, column=0)
-        
         self.dataPanel.pack(side = tk.TOP)
         self.butPanel.pack(side = tk.LEFT)
-        self.pack()
-        self.Ui.mainloop()
-        
-
-
-
-    def buildStartUI(self):
-        self.funny = tk.Frame(self)
-
-
-        self.labelMainText = tk.Label(self.funny, text="Select which menu to view.")
-        self.labelMainText.grid(row=0, columnspan=2)
-
-        self.btnCompleted = tk.Button(self.funny, text="Completed Actions", command=self.buildCompletedUI)
-        self.btnCompleted.grid(row=1, column=0)
-
-        self.btnUnCompleted = tk.Button(self.funny, text="Uncompleted Actions", command=self.buildUncompletedUI)
-        self.btnUnCompleted.grid(row=1, column=1)
-
-        self.funny.pack(side=tk.TOP)
-        self.pack()
+        self.settingPanel.grid(row=2, column=2)
 
 
 
 
 
-
-#TODO: (HAHA GET IT!)
-#!    - Utilize a radio button to toggle between completed and uncompleted. 
 
 root = tk.Tk()
 #root.geometry("800x500") #* temporary window size
-
-app = TkinterApp()
-app.master.title("Acto")
-app.mainloop()
+app = TkinterApp(root)
+root.mainloop()
